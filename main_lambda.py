7c30407fdd2f59779b0eec53806580987f6af1e2
Abdogouhmad <gouhmada@gmail.com>
2024-06-26 19:58:15 +0100

    Working on new feature 👷‍♂️

--- main_lambda.py
+++ main_lambda.py
@@ -51,9 +51,49 @@ def filter_data_pandas(data: Union[dict, str]) -> str | None:
         return json.dumps(data, indent=4)  # Return error message as JSON
 
 
+def filter_into_column(data: Union[dict, str]):
+    """
+    Filter the data and return it in table form.
+
+    Args:
+        data (Union[dict, str]): The user data to filter, expected to be a dictionary.
+
+    Returns:
+        pd.DataFrame or str: Returns a DataFrame containing filtered data or an error message.
+    """
+    if isinstance(data, dict) and "posts" in data:
+        posts = data["posts"]
+        if posts:
+            # Extracting only relevant fields for each post
+            filtered_data = [
+                {
+                    "Likes": post["reactions"]["likes"],
+                    "Dislikes": post["reactions"]["dislikes"],
+                    "Views": post["views"],
+                }
+                for post in posts
+            ]
+            df = pd.DataFrame(filtered_data)
+            return df
+        else:
+            return json.dumps({"message": "No data found"})
+    else:
+        return None  # Handle invalid data gracefully
+
+
 if __name__ == "__main__":
-    user_id = input("Enter the id user to get results: ")
+    user_id = input("Enter the user ID to fetch data: ")
     URL = f"https://dummyjson.com/posts/user/{user_id}"
     data = get_user(URL)
-    filtered_data = filter_data_pandas(data)
-    print(filtered_data)  # Print or return the JSON response
+    the_table = filter_into_column(data)
+    the_json = filter_data_pandas(data)
+    if isinstance(the_table, pd.DataFrame):
+        print("Into table")
+        print()
+        print(the_table)
+        print()
+        print("Into json")
+        print()
+        print(the_json)
+    else:
+        print(the_table)  # Print or return the JSON error message
